bf865ff887da687151271747417c1460c168f184
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-02-02 04:34:43 +0700

    Added Pairwise loss and some refactoring

--- train_reward_model.py
+++ train_reward_model.py
@@ -1,67 +1,48 @@
-import torch
+import comet_ml
 import hydra
+from omegaconf import DictConfig
+from prepare_dataset import prepare_dataset
+from reward_model_pairwised import PairwisedRewardTrainer
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from trl import RewardConfig, RewardTrainer
 
-from transformers import  (
-    AutoModelForSequenceClassification, 
-    AutoTokenizer, 
-    HfArgumentParser
-)
-
-from trl import (
-    ModelConfig,
-    RewardConfig,
-    RewardTrainer,
-    ScriptArguments,
-    get_kbit_device_map,
-    get_quantization_config,
-    setup_chat_format,
-)
-
-from src.dataset import prepare_dataset
+from utils.init_utils import set_random_seed, set_worker_seed
 
 
-@hydra.main(version_base=None, config_path="configs", config_name="reward_modeling")
-def main(config):
-    parser = HfArgumentParser((ScriptArguments, RewardConfig, ModelConfig))
-    script_args, training_args, model_args = parser.parse_args_into_dataclasses()
+@hydra.main(version_base=None, config_path="configs", config_name="reward_model")
+def main(config: DictConfig):
+    set_random_seed(42)
+    set_worker_seed(0)
+    comet_ml.login(project_name="tlab-reinforce-reward-model")
+
+    training_args = RewardConfig(**config.trainer.training_args)
     training_args.gradient_checkpointing_kwargs = dict(use_reentrant=False)
 
-    quantization_config = get_quantization_config(model_args)
-    model_kwargs = dict(
-        revision=model_args.model_revision,
-        device_map=get_kbit_device_map() if quantization_config is not None else None,
-        quantization_config=quantization_config,
-        use_cache=False if training_args.gradient_checkpointing else True,
-        torch_dtype=torch.bfloat16,
-    )
-    
     tokenizer = AutoTokenizer.from_pretrained(
-        model_args.model_name_or_path, 
-        trust_remote_code=model_args.trust_remote_code, 
-        use_fast=True
+        config.reward_model.model_name, **config.reward_model.model_params
     )
     model = AutoModelForSequenceClassification.from_pretrained(
-        model_args.model_name_or_path, 
-        num_labels=1, 
-        trust_remote_code=model_args.trust_remote_code, 
-        **model_kwargs
+        config.reward_model.model_name,
+        **config.reward_model.model_params,
     )
-
     model.config.pad_token_id = tokenizer.pad_token_id
-
-    if tokenizer.chat_template is None:
-        model, tokenizer = setup_chat_format(model, tokenizer)
 
     _, _, train_ds, test_ds = prepare_dataset(**config.dataset)
 
- 
-    trainer = RewardTrainer(
+    TrainerClass = (
+        PairwisedRewardTrainer
+        if config.reward_model.model_params.num_labels != 1
+        else RewardTrainer
+    )
+
+    trainer = TrainerClass(
         model=model,
         processing_class=tokenizer,
         args=training_args,
         train_dataset=train_ds,
-        eval_dataset=test_ds
+        eval_dataset=test_ds,
     )
+
     trainer.train()
 
     if training_args.eval_strategy != "no":
@@ -69,7 +50,9 @@ def main(config):
         trainer.log_metrics("eval", metrics)
         trainer.save_metrics("eval", metrics)
 
+    if training_args.push_to_hub:
+        trainer.push_to_hub(dataset_name=config.dataset.dataset_name)
+
 
 if __name__ == "__main__":
     main()
-
